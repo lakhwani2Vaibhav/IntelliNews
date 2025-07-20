f850caea1b5a8b2cbe63b0a195014d0b2161b79b
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-07-20 17:58:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'nis-gs.pix.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
